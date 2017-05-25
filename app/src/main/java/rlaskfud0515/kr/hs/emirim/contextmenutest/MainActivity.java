3d3d1a6640402636965d49d2f2b5fccc607ac9e1
every-sushi <nadia0515@naver.com>
2017-05-26 04:30:11 +0900

    #4아나 개빡치넨에 포맷을 해도 일러때문에에 못해애 내일해야돼애 말이돼?

--- app/src/main/java/rlaskfud0515/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/rlaskfud0515/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -1,20 +1,28 @@
 package rlaskfud0515.kr.hs.emirim.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but1,but2;
+    ImageView imgV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+         but1=(Button)findViewById(R.id.but1);
+         but2=(Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear);//이게 있어야 백그라운드 컬러를 바꾸져
+        imgV=(ImageView)findViewById(R.id.img1);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -30,6 +38,36 @@ public class MainActivity extends AppCompatActivity {
         }
         if(v.getId()==R.id.but2)
             menuInflater.inflate(R.menu.menu2, menu);
+    }
+//메소드 바깥에서 선언된 변수를 필드
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        super.onContextItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+
+            case R.id.menu1_item_pink:
+                linear.setBackgroundColor(Color.rgb(255,0,127));
+
+                return true;
+
+            case R.id.menu2_item_size:
+                imgV.setScaleX(imgV.getRotation()+90);
+                return true;
+
+            case R.id.menu2_item_rotation:
+                imgV.setRotation(imgV.getScaleX()*2);
+                return true;
+
+        }
+
+        return false;
     }
 
 }
